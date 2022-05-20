9038fed9384ca17b87c3c4472129c0949b450ac7
sdmnny <sdmnny@gmail.com>
2022-05-19 20:14:44 -0400

    edited some styles to css

--- assests/js/script.js
+++ assests/js/script.js
@@ -33,3 +33,28 @@ $(".resetBtn").on("click", function (){
 );
 
 
+$("#hour1 .description").val(localStorage.getItem("hour1"));
+$("#hour1 .description").val(localStorage.getItem("hour2"));
+$("#hour1 .description").val(localStorage.getItem("hour3"));
+$("#hour1 .description").val(localStorage.getItem("hour4"));
+$("#hour1 .description").val(localStorage.getItem("hour5"));
+$("#hour1 .description").val(localStorage.getItem("hour6"));
+$("#hour1 .description").val(localStorage.getItem("hour7"));
+$("#hour1 .description").val(localStorage.getItem("hour8"));
+$("#hour1 .description").val(localStorage.getItem("hour9"));
+$("#hour1 .description").val(localStorage.getItem("hour10"));
+$("#hour1 .description").val(localStorage.getItem("hour11"));
+$("#hour1 .description").val(localStorage.getItem("hour12"));
+$("#hour1 .description").val(localStorage.getItem("hour13"));
+$("#hour1 .description").val(localStorage.getItem("hour14"));
+$("#hour1 .description").val(localStorage.getItem("hour15"));
+$("#hour1 .description").val(localStorage.getItem("hour16"));
+$("#hour1 .description").val(localStorage.getItem("hour17"));
+$("#hour1 .description").val(localStorage.getItem("hour18"));
+$("#hour1 .description").val(localStorage.getItem("hour19"));
+$("#hour1 .description").val(localStorage.getItem("hour20"));
+$("#hour1 .description").val(localStorage.getItem("hour21"));
+$("#hour1 .description").val(localStorage.getItem("hour22"));
+$("#hour1 .description").val(localStorage.getItem("hour23"));
+$("#hour1 .description").val(localStorage.getItem("hour24"));
+
