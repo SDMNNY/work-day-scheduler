f7ed28dd578580b03eb99b3e32519f7717b5bd13
sdmnny <sdmnny@gmail.com>
2022-05-19 20:06:59 -0400

    created for loop in script file

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,2 +1,35 @@
 $("#currentDay").text(moment().format("dddd, MMMM do, YYYY")); 
 
+var currentTime = moment.hour();
+
+for (i = 0; i < 25; i++) {
+    if (currentTime ===i) {
+        $(".container").children().eq(i).children().eq(1).addClass("present");
+    } else if (currentTime > i) {
+        $(".container").children().eq(i).children().eq(1).addClass("past");
+    } else {
+        $(".container").children().eq(i).children().eq(1).addClass("future");
+    }
+
+}
+
+
+
+$(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var hour = $(this).parent().attr("id");
+
+    localStorage.setItem(hour, text);
+
+}
+);
+
+
+$(".resetBtn").on("click", function (){
+    localStorage.clear();
+    location.reload();
+
+}
+);
+
+
